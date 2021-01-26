3347eceb4250adf96857fa6a849459e1e4329030
Chris Athanas <chris@crowdskout.com>
2021-01-26 15:09:41 -0600

    Add completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { useReducer, useState, useEffect } from 'react'
 
 const ACTIONS = {
-  ADD_TODO: 'add-todo'
+  ADD_TODO: 'add-todo',
+  COMPLETE_TODO: 'complete-todo'
 }
 
 function reducer( todos, action ) {
   console.log({todos}, {action})
-  switch (action.type) {
+  switch (action.eventType) {
     case ACTIONS.ADD_TODO:
       return [...todos, newTodo(action.payload.name)]
+    case ACTIONS.COMPLETE_TODO:
+      return completeTodo(todos, action.payload.indexToComplete)
     default:
       return [todos]
   }
 }
-
 
 function newTodo(name) {
   return { 
@@ -23,14 +25,36 @@ function newTodo(name) {
   }
 }
 
+function completeTodo(todos, indexToComplete) {
+  indexToComplete = parseInt(indexToComplete)
+  const newTodos = todos.map( (todo, index) => {
+     if (index === indexToComplete) {
+       todo.completed = true
+       return todo
+     } else {
+       return todo
+     }
+  })
+
+  return newTodos
+}
+
 function App() {
   const [todos, dispatch] = useReducer( reducer, [])
   const [nameState, setNameState] = useState('')
 
   function handleSubmit(e) {
     e.preventDefault()
-    dispatch({ type: ACTIONS.ADD_TODO, payload: {name: nameState} })
-    setNameState('')
+
+    if (nameState.includes("COMP")) {
+      let indexToComplete = nameState.split(' ')[1]
+      dispatch({ eventType: ACTIONS.COMPLETE_TODO, payload: {indexToComplete: indexToComplete} })
+      setNameState('')
+    } else {
+      dispatch({ eventType: ACTIONS.ADD_TODO, payload: {name: nameState} })
+      setNameState('')
+    }
+
   }
 
   useEffect( () => {
