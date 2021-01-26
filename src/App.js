6c269f1277771d84f5801164190404da2cf01594
Chris Athanas <chris@crowdskout.com>
2021-01-26 13:50:21 -0600

    Add reducer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from 'react'
+
+const ACTIONS = {
+  ADD_TODO: 'add-todo'
+}
+
+function reducer( todos, action ) {
+  console.log({todos}, {action})
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)]
+    default:
+      return [todos]
+  }
+}
+
+
+function newTodo(name) {
+  return { 
+    id: Date.now(),
+    name: name,
+    completed: false
+  }
+}
 
 function App() {
+  const [todos, dispatch] = useReducer( reducer, [])
+  const [nameX, setNameX] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    dispatch({ type: ACTIONS.ADD_TODO, payload: {name: nameX} })
+    setNameX('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" 
+               value={nameX} 
+               onChange={e => setNameX(e.target.value)} />
+      </form>
+    </>
   );
 }
 
